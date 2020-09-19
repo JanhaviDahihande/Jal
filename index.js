951d89177c40cb867092201ea68df0efdc02b712
janhavi <janhavi.dahihande@oculid.com>
2020-09-19 15:11:19 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const notifier = require('node-notifier');
+const path = require('path');
+
+var intervalInSeconds = 10;
+var refreshIntervalId = setInterval(showNotification, intervalInSeconds * 1000);
+
+const reminderMessages = [
+    'Give your body the water it needs! Take a sip!',
+    'Take a sip!',
+    'Time to drink water!',
+    'Put some fluid in your body!'
+];
+
+function getRandomArbitrary() {
+    const max = reminderMessages.length;
+    return reminderMessages[Math.floor(Math.random() * max)];
+}
+
+function showNotification() {
+    console.log("object");
+    notifier.notify(
+        {
+          title: 'Jal 💦',
+          message: getRandomArbitrary(),
+          icon: path.join(__dirname, 'drop.png'),
+          sound: true,
+          wait: true,
+          actions: ['Yes'],
+          closeLabel: 'Stop'
+        },
+        function (err, response) {
+          if (response == 'closed') {
+            console.log("You opted out from reminders :(");
+            clearInterval(refreshIntervalId);
+          }
+        }
+    );
+}
